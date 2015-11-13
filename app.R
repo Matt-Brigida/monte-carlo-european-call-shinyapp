57d434c3e3b39abdf3924018b72c3235ba041e1c
Matt Brigida <matt@complete-markets.com>
2015-11-13 11:44:00 -0500

    changed to multiple monte carlo runs

--- app.R
+++ app.R
@@ -1,14 +1,24 @@
 library(shiny)
 
 ui <- bootstrapPage(
-		    sliderInput(inputId = "num", label = "Amount of Random Numbers", min = 1000, max = 10000000, step = 10000, value = 100000)
+		    sliderInput(inputId = "reps", label = "Number of Monte Carlo Runs", min = 50, max = 500, step = 1, value = 1),
+		    plotOutput("distPlot")
     )
     
 server <- function(input, output){
 
 output$distPlot <- renderPlot({ 
-
-
+S <- 50
+K <- 45
+r <- 0.01
+vol <- 0.2
+T <- 0.5
+call <- 0
+for (i in 1:input$reps){
+ z <- rnorm(1000000)
+ call[i] <- exp(-r*T)*mean(ifelse(S*exp((r-.5*vol^2)*T+vol*sqrt(T)*z)>K, S*exp((r-.5*vol^2)*T+vol*sqrt(T)*z)-K, 0))
+ }
+plot(density(call), main = "Monte Carlo Value of a European Call", sub = "Call Option Value")
 })
 
 
